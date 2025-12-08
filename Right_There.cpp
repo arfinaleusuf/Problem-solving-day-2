db96f7c8785e129fd30eb461931a132a26734dab
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-12-09 01:39:38 +0600

    Right there

--- Right_There.cpp
+++ Right_There.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 int main()
 {
-    cout << "hello world";
+    int t;
+    cin>>t;
+    for(int i = 0; i<t; i++)
+    {
+        int n,x;
+        cin>>n >>x;
+        if(n<=x)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
     return 0;
 }
